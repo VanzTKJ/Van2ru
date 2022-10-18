30c258ee8acf4cda054680f2c23db96b6888d1e1
nafron <99122428+nafron@users.noreply.github.com>
2022-10-18 13:50:57 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`${global.db.data.users[who].limit} Limit Left=͟͟͞͞🏀`)
+    m.reply(`*${global.db.data.users[who].limit}* *ꜱɪꜱᴀ ʟɪᴍɪᴛ ᴋᴀᴍᴜ* =͟͟͞͞🏀`)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
