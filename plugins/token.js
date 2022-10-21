ebfb0405bcd3e51ba05ba50370d5238204c4766b
nafron <99122428+nafron@users.noreply.github.com>
2022-10-22 06:26:33 +0700

    Update token.js

--- plugins/token.js
+++ plugins/token.js
@@ -3,7 +3,8 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
-    m.reply(`*${global.db.data.users[who].token}* *ꜱɪꜱᴀ ᴛᴏᴋᴇɴ ᴋᴀᴍᴜ* =͟͟͞͞🏀`)
+    m.reply(`*${global.db.data.users[who].token}* *ꜱɪꜱᴀ ᴛᴏᴋᴇɴ ᴋᴀᴍᴜ* =͟͟͞͞🏀
+    ᴋᴇᴛɪᴋ ᴘᴇʀɪɴᴛᴀʜ #token ᴜɴᴛᴜᴋ ᴄᴇᴋ ᴛᴏᴋᴇɴ ᴋᴀᴍᴜ`)
 }
 handler.help = ['token [@user]']
 handler.tags = ['xp']
