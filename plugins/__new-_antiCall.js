9f5b79adc5389459d41306a21dc496e4cfb0f703
nafron <99122428+nafron@users.noreply.github.com>
2022-11-04 17:32:20 +0700

    Update __new-_antiCall.js

--- plugins/__new-_antiCall.js
+++ plugins/__new-_antiCall.js
@@ -14,7 +14,7 @@ export async function all(m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await this.reply(m.chat, '𝐒𝐢𝐬𝐭𝐞𝐦 𝐚𝐮𝐭𝐨  𝐛𝐥𝐨𝐜𝐤𝐢𝐫 𝐨𝐥𝐞𝐡 𝐛𝐨𝐭 𝐤𝐚𝐫𝐞𝐧𝐚 𝐭𝐞𝐥𝐚𝐡 𝐦𝐞𝐥𝐚𝐧𝐠𝐠𝐚𝐫 𝐚𝐭𝐮𝐫𝐚𝐧 𝐛𝐨𝐭\𝐧\𝐧*📮𝐃𝐢𝐥𝐚𝐫𝐚𝐧𝐠 𝐦𝐞𝐧𝐞𝐥𝐞𝐩𝐨𝐧 𝐛𝐨𝐭!*,Hubungin pemilik wa.me/6281252733108/bang-saya-minta-buka-blokir-botnya-janji-gak-nelfon-lagi', null)
+		await this.reply(m.chat, '𝐒𝐢𝐬𝐭𝐞𝐦 𝐚𝐮𝐭𝐨  𝐛𝐥𝐨𝐜𝐤𝐢𝐫 𝐨𝐥𝐞𝐡 𝐛𝐨𝐭 𝐤𝐚𝐫𝐞𝐧𝐚 𝐭𝐞𝐥𝐚𝐡 𝐦𝐞𝐥𝐚𝐧𝐠𝐠𝐚𝐫 𝐚𝐭𝐮𝐫𝐚𝐧 𝐛𝐨𝐭\𝐧\𝐧*📮𝐃𝐢𝐥𝐚𝐫𝐚𝐧𝐠 𝐦𝐞𝐧𝐞𝐥𝐞𝐩𝐨𝐧 𝐛𝐨𝐭!*,Hubungin pemilik wa.me/6281252733108/text=kena+blokir', null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
