5e94afb866eaec00c20a65f1eb51221d88812aa9
Vannzzbit <elvanoadepratama@gmail.com>
2023-03-12 08:56:59 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -1,28 +1,26 @@
-__path = process.cwd()
-module.exports = async (md, m, fetchJson, bot, type, body, budy, prefix, isCommand, command, isGroup, md1, pushname, q, args, content, sender, from, nama, namabot, owner, reply, donasi, isMedia, isQuotedImage, isQuotedVideo, isQuotedSticker, isQuotedAudio) => {  
+import { Configuration, OpenAIApi } from "openai";
+import fetch from "node-fetch"
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
 
-if (!q) return md.sendMessage(from, {text: "Masukkan text"}, {quoted: md1})
-
-const { Configuration, OpenAIApi } = require("openai");
-
+import fs from 'fs'
+let handler = async (m, { conn, text }) => {
+if (!text) throw "Hay adakah yang bisa saya bantu??"
 const configuration = new Configuration({
-  apiKey: "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",
+    apiKey: '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'
 });
 const openai = new OpenAIApi(configuration);
-try {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: q,
-    max_tokens: 2000,
-    temperature: 0,
-  });
-  md.sendMessage(from, {text: completion.data.choices[0].text}, {quoted: md1})
-} catch (error) {
-  if (error.response) {
-    console.log(error.response.status);
-    console.log(error.response.data);
-  } else {
-    console.log(error.message);
-  }
-}
-} 
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: text,
+            temperature: 0,
+            max_tokens: 2000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        });
+m.reply(response.data.choices[0].text)
+    }
+handler.help = ['openai']
+handler.tags = ['Tool']
+ handler.command = /^(yus|ai|openai)$/i
+export default handler
