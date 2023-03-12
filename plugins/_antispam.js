ea32e3441b790d67ef7c9c40371c2379295c92ad
Vannzzbit <elvanoadepratama@gmail.com>
2023-03-12 07:57:39 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -7,7 +7,7 @@ export async function all(m) {
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
             if (this.spam[m.sender].count > 5) {
                 global.db.data.users[m.sender].banned = true
-                m.reply('*JANGAN SPAM !!!*\n\n*💬Laporkan masalah ini ke wa.me/6281252733108?text=📧Bang+saya+ingin+membuka+banned,+saya+akan+membayar+pajak+1000')
+                m.reply('*JANGAN SPAM !!!*\n\n*💬Laporkan masalah ini ke wa.me/62895801290205?text=📧Bang+saya+ingin+membuka+banned,+saya+akan+membayar+pajak+1000')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
