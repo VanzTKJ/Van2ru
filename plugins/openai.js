dd501ed1e6334ea006c7e20b29ec16a559889cee
Vannzzbit <elvanoadepratama@gmail.com>
2023-03-18 15:53:51 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -1,26 +1,15 @@
-import { Configuration, OpenAIApi } from "openai";
-import fetch from "node-fetch"
-import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+var tiodev = require("node-fetch")
+var handler = async (m, {
+ text, 
+ usedPrefix, 
+ command
+ }) => {
+    if (!text) throw `Contoh:\n${usedPrefix + command} berikan contoh kode html`
 
-import fs from 'fs'
-let handler = async (m, { conn, text }) => {
-if (!text) throw "Hay adakah yang bisa saya bantu??"
-const configuration = new Configuration({
-    apiKey: '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'
-});
-const openai = new OpenAIApi(configuration);
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: text,
-            temperature: 0,
-            max_tokens: 2000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0
-        });
-m.reply(response.data.choices[0].text)
-    }
-handler.help = ['openai']
-handler.tags = ['Tool']
- handler.command = /^(yus|ai|openai)$/i
-export default handler
+var tiores = await tiodev(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
+ let hasil = await tiores.json()
+ m.reply(`${hasil.data.data}`.trim())
+    };  
+handler.command = handler.help = ['ai'];
+handler.tags = ['internet'];
+module.exports = handler;
